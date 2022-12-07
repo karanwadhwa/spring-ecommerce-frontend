f3b263f41e16758a4c23f9f65a7e2a4a54c4f706
Karan Wadhwa <wadhwa.k@northeastern.edu>
2022-12-07 17:02:42 -0500

    product action creators and reducer

--- src/store/actions/ProductActions.js
+++ src/store/actions/ProductActions.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 import { apiURL } from "../../variable";
-import { SET_ALL_PRODUCTS } from "../types";
+import {
+  ADD_TO_CART,
+  CLEAR_CART,
+  REMOVE_FROM_CART,
+  SET_ALL_PRODUCTS,
+} from "../types";
 
 export const getAllProducts = () => (dispatch) => {
   axios
@@ -12,4 +17,39 @@ export const getAllProducts = () => (dispatch) => {
       console.log(error, error.response);
       alert("Error fetching products!");
     });
+};
+
+export const addToCart = (product, cartQuantity) => {
+  return { type: ADD_TO_CART, payload: { ...product, cartQuantity } };
+};
+
+export const removeFromCart = (id) => {
+  return { type: REMOVE_FROM_CART, payload: id };
+};
+
+export const clearCart = () => {
+  return { type: CLEAR_CART };
+};
+
+export const orderCheckout = (cart, userid) => (dispatch) => {
+  console.log(cart, userid);
+  const orderTotal = parseFloat(cart.cartTotal.toFixed(2));
+  const status = "booked";
+  const items = cart.items.forEach(({ name, price, cartQuantity }) => ({
+    name,
+    price,
+    quantity: cartQuantity,
+    totalPrice: parseFloat((cartQuantity * price).toFixed(2)),
+  }));
+  axios
+    .put(`${apiURL}/user/${userid}/order/create`, {
+      items,
+      status,
+      orderTotal,
+    })
+    .then((res) => {
+      console.log(res);
+      dispatch(clearCart);
+    })
+    .catch((err) => console.log(err, err.response));
 };
--- src/store/reducers/productReducer.js
+++ src/store/reducers/productReducer.js
@@ -1,13 +1,34 @@
-import { SET_ALL_PRODUCTS } from "../types";
+import {
+  ADD_TO_CART,
+  CLEAR_CART,
+  LOGOUT,
+  REMOVE_FROM_CART,
+  SET_ALL_PRODUCTS,
+} from "../types";
 
 const initialState = {
   allItems: [],
+  cart: [],
+  orders: [],
 };
 
 export default (state = initialState, action) => {
+  let cart = [...state.cart];
   switch (action.type) {
     case SET_ALL_PRODUCTS:
       return { ...state, allItems: action.payload };
+    case ADD_TO_CART:
+      const index = cart.findIndex((i) => i.id === action.payload.id);
+      if (index === -1) cart.push(action.payload);
+      else cart[index] = action.payload;
+      return { ...state, cart };
+    case REMOVE_FROM_CART:
+      cart = state.cart.filter((i) => i.id !== action.payload);
+      return { ...state, cart };
+    case CLEAR_CART:
+      return { ...state, cart: [] };
+    case LOGOUT:
+      return initialState;
     default:
       return initialState;
   }
